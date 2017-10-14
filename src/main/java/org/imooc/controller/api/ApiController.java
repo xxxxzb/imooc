a69209f2d72baf7d793be40f32efeed5b09ca328
xzb <xzb@192.168.1.100>
2017-10-14 10:33:20 +0800

    ..

--- src/main/java/org/imooc/controller/api/ApiController.java
+++ src/main/java/org/imooc/controller/api/ApiController.java
@@ -4,29 +4,27 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.websocket.server.PathParam;
-
-import org.imooc.bean.Ad;
-import org.imooc.bean.BusinessList;
 import org.imooc.bean.Comment;
 import org.imooc.bean.Orders;
 import org.imooc.bean.Page;
 import org.imooc.constant.ApiCodeEnum;
-import org.imooc.controller.content.BusinessesController;
 import org.imooc.dto.AdDto;
 import org.imooc.dto.ApiCodeDto;
 import org.imooc.dto.BusinessDto;
 import org.imooc.dto.BusinessListDto;
+import org.imooc.dto.CommentForSubmitDto;
+import org.imooc.dto.CommentListDto;
 import org.imooc.dto.OrderForBuyDto;
 import org.imooc.dto.OrdersDto;
 import org.imooc.service.content.AdService;
 import org.imooc.service.content.BusinessService;
+import org.imooc.service.content.CommentService;
 import org.imooc.service.content.MemberService;
+import org.imooc.service.content.OrdersService;
+import org.imooc.task.BusinessTask;
 import org.imooc.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,6 +50,12 @@ public class ApiController {
 	@Autowired
 	private MemberService memberService;
 	
+	@Autowired
+	private OrdersService ordersService;
+	
+	@Autowired
+	private CommentService commentService;
+	
 	@Value("${ad.number}")
 	private int adNumber;
 	
@@ -63,9 +67,6 @@ public class ApiController {
 	
 	/**
 	 * 首页 —— 广告（超值特惠）
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
 	 */
 	@RequestMapping(value="/homead",method=RequestMethod.GET)
 	public List<AdDto> homead(){
@@ -76,9 +77,6 @@ public class ApiController {
 	
 	/**
 	 * 首页 —— 推荐列表（猜你喜欢）
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
 	 */
 	@RequestMapping(value="/homelist/{city}/{page}",method=RequestMethod.GET)
 	public BusinessListDto homelist() {
@@ -89,11 +87,7 @@ public class ApiController {
 	
 	/**
 	 * 搜索结果页 - 搜索结果 - 三个参数
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
 	 */
-	@ResponseBody
 	@RequestMapping(value="/search/{page}/{city}/{category}/{keyword}",method=RequestMethod.GET)
 	public BusinessListDto searchByThree(BusinessDto businessDto){
 		businessDto.getPage().setPageNumber(businessSearchNumber);
@@ -102,9 +96,6 @@ public class ApiController {
 	
 	/**
 	 * 搜索结果页 - 搜索结果 - 两个参数
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
 	 */
 	@RequestMapping(value="/search/{page}/{city}/{category}",method=RequestMethod.GET)
 	public BusinessListDto searchByTwo(BusinessDto businessDto){
@@ -114,54 +105,11 @@ public class ApiController {
 	
 	/**
 	 * 详情页 - 商户信息
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
 	 */
 	@RequestMapping(value="/detail/info/{id}",method=RequestMethod.GET)
 	public BusinessDto detailInfo(@PathVariable("id")Long id){
 		return businessService.seachById(id);
 	}
-	
-	/**
-	 * 详情页 - 用户评论
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
-	 */
-	@RequestMapping(value="/detail/comment/{page}/{id}",method=RequestMethod.GET)
-	public Comment detailComment(@PathVariable("id")Long id,Page page){
-		return null;
-	}
-	
-	
-	/**
-	 * 获取提交评论
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
-	 */
-	@RequestMapping(value="/submitComment",method=RequestMethod.POST)
-	public Map<String,Object> submitComment() {
-		Map<String,Object> result=new HashMap<String,Object>();
-		result.put("errno", 0);
-		result.put("msg", "ok");
-		return result;
-	}
-	
-	/**
-	 * 订单列表
-	 * @throws IOException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
-	 */
-	@RequestMapping(value="/orderlist/{username}",method=RequestMethod.GET)
-	public Orders orderlist() throws JsonParseException, JsonMappingException, IOException{
-		ObjectMapper mapper=new ObjectMapper();
-		String s="";
-		return mapper.readValue(s, new TypeReference <Orders>(){});
-	}
-	
 	
 	/**
 	 * 根据手机号 下发短信验证码
@@ -223,6 +171,7 @@ public class ApiController {
 	 */
 	@RequestMapping(value="/order",method=RequestMethod.POST)
 	public ApiCodeDto order(OrderForBuyDto orderForBuyDto) {
+		ApiCodeDto dto;
 		//1、校验token是否有效(缓存中是否存在这样一个token,并且对应存放的会员信息(这里指手机号)与提交上来的信息一致)
 		Long phone = memberService.getPhone(orderForBuyDto.getToken());
 		if(phone!=null && phone.equals(orderForBuyDto.getUsername())) {
@@ -234,8 +183,58 @@ public class ApiController {
 			ordersDto.setPrice(orderForBuyDto.getPrice());
 			ordersDto.setBusinessId(orderForBuyDto.getId());
 			ordersDto.setMemberId(memberId);
-			
+			ordersService.add(ordersDto);
+//			BusinessTask businessTask= new BusinessTask();
+//			businessTask.synNumber();
+			dto = new ApiCodeDto(ApiCodeEnum.SUCCESS);
+		}else{
+			dto = new ApiCodeDto(ApiCodeEnum.NOT_LOGGED);
 		}
-		return null;
+		return dto;
+	}
+	
+	/**
+	 * 订单列表 
+	 */
+	@RequestMapping(value="/orderlist/{username}",method=RequestMethod.GET)
+	public List<OrdersDto> orderlist(@PathVariable("username")Long username) {
+		Long memberId = memberService.getIdByPhone(username);
+		return ordersService.getListByMemberId(memberId);
+	}
+	
+	/**
+	 * 提交评论
+	 */
+	@RequestMapping(value="/submitComment",method=RequestMethod.POST)
+	public ApiCodeDto submitComment(CommentForSubmitDto dto){
+		ApiCodeDto apiCodeDto;
+		//1、校验登录信息：token、手机号
+		Long phone = memberService.getPhone(dto.getToken());
+		if(phone !=null && dto.getUsername().equals(phone)){			
+		//2、根据手机号获取会员id
+			Long memberId = memberService.getIdByPhone(phone);
+		// 3、根据提交上来的订单ID获取对应的会员ID，校验与当前登录的会员是否一致
+			OrdersDto ordersDto = ordersService.getbyId(dto.getId());
+			if(ordersDto.getMemberId().equals(memberId)){
+				//4、保存评论
+				commentService.add(dto);
+				//TODO 5、非实时同步星星数
+				apiCodeDto = new ApiCodeDto(ApiCodeEnum.SUCCESS);
+			}else{
+				apiCodeDto = new ApiCodeDto(ApiCodeEnum.NO_AUTH);
+			}
+		}else{
+			apiCodeDto = new ApiCodeDto(ApiCodeEnum.NOT_LOGGED);
+		}
+		return apiCodeDto;
+	}
+	
+	/**
+	 * TODO
+	 * 详情页 - 用户评论
+	 */
+	@RequestMapping(value = "/detail/comment/{currentPage}/{businessId}", method = RequestMethod.GET)
+	public CommentListDto detail(@PathVariable("businessId") Long businessId,Page page) {
+		return commentService.getListByBusinessId(businessId,page);
 	}
 }
